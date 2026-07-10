b1c2f35b026ee6469c93823d73d8faac7618d3c5
anton <anton.azizi1@gmail.com>
2026-07-10 22:40:30 +0200

    basic jwt auth configuration

---
+++ api/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+using api.DTOs.Auth;
+using api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            var result = await _authService.RegisterAsync(dto);
+
+            if (result.Contains("already"))
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var token = await _authService.LoginAsync(dto);
+
+            if (token == null)
+                return Unauthorized("Invalid username or password");
+
+            return Ok(new
+            {
+                token
+            });
+        }
+    }
+}
---
+++ api/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,11 @@
+namespace api.DTOs.Auth
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
